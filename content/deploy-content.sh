57371914b2f1803bc8dbfc0fd9a1f13d732a0163
Douglas Naphas <douglasnaphas@gmail.com>
2022-11-27 14:36:05 -0500

    Deploy content to all buckets

--- content/deploy-content.sh
+++ content/deploy-content.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 STACKNAME=$(npx @cdk-turnkey/stackname@1.2.0 --suffix app)
-BUCKET_NAME=$(aws cloudformation describe-stacks \
-  --stack-name ${STACKNAME} | \
-  jq '.Stacks[0].Outputs | map(select(.OutputKey == "BucketName"))[0].OutputValue' | \
-  tr -d \")
 mkdir -p deploy
 cp *.json deploy/
-aws s3 sync \
-  --content-type "application/json" \
-  --delete \
-  deploy/ \
-  s3://${BUCKET_NAME}
+for k in BucketName PublicSecureBucketName PublicInsecureBucketName
+do
+  BUCKET_NAME=$(aws cloudformation describe-stacks \
+    --stack-name ${STACKNAME} | \
+    jq '.Stacks[0].Outputs | map(select(.OutputKey == "${k}"))[0].OutputValue' | \
+    tr -d \")
+  aws s3 sync \
+    --content-type "application/json" \
+    --delete \
+    deploy/ \
+    s3://${BUCKET_NAME}
+done
