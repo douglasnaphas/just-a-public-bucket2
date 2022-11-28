cef0b7ed1468be91c4683423106082a3cbd2d98a
Douglas Naphas <douglasnaphas@gmail.com>
2022-11-27 21:52:05 -0500

    Remove debugging output

--- content/deploy-content.sh
+++ content/deploy-content.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 STACKNAME=$(npx @cdk-turnkey/stackname@1.2.0 --suffix app)
-echo "STACKNAME: ${STACKNAME}"
 mkdir -p deploy
 cp *.json deploy/
 for k in BucketName PublicSecureBucketName PublicInsecureBucketName
 do
-  echo "k: ${k}"
   BUCKET_NAME=$(aws cloudformation describe-stacks \
     --stack-name ${STACKNAME} | \
     jq --arg k $k '.Stacks[0].Outputs | map(select(.OutputKey == $k))[0].OutputValue' | \
     tr -d \")
-  echo "BUCKET_NAME: ${BUCKET_NAME}"
   aws s3 sync \
     --content-type "application/json" \
     --delete \
